fb7359e755004baf830dbd7e0eef4bb2e8bb47cc
Jeff McBride <mcbridejc@gmail.com>
2014-10-07 18:40:32 -0700

    Add support for flushing to mtd block devices

--- drivers/mtd/mtd_blkdevs.c
+++ drivers/mtd/mtd_blkdevs.c
@@ -89,6 +89,12 @@ static int do_blktrans_request(struct mtd_blktrans_ops *tr,
 	if (req->cmd_type != REQ_TYPE_FS)
 		return -EIO;
 
+	if (req->cmd_flags & REQ_FLUSH) {
+		if (tr->flush)
+			return tr->flush(dev);
+		return 0;
+	}
+
 	if (blk_rq_pos(req) + blk_rq_cur_sectors(req) >
 	    get_capacity(req->rq_disk))
 		return -EIO;
@@ -408,6 +414,8 @@ int add_mtd_blktrans_dev(struct mtd_blktrans_dev *new)
 
 	if (!new->rq)
 		goto error3;
+
+	blk_queue_flush(new->rq, REQ_FLUSH);
 
 	new->rq->queuedata = new;
 	blk_queue_logical_block_size(new->rq, tr->blksize);
